023b32dce4357d4924d482c82acb1e468b70f84c
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-03-13 14:41:14 +0700

    RecyclerView, FloatingActionButton and Store Data into SQLite using Room (ORM model)

---
+++ app/src/main/java/com/example/contactapp/ContactDao.java
@@ -0,0 +1,16 @@
+package com.example.contactapp;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    @Query("SELECT * FROM Contact")
+    List<Contact> getAllContacts();
+
+    @Insert
+    void insertAll(Contact... contact);
+}
--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.contactapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ArrayList<Contact> contacts;
+    private ContactsAdapter contactsAdapter;
+
+    private AppDatabase appDatabase;
+    private ContactDao contactDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        appDatabase = AppDatabase.getInstance(this);
+        contactDao = appDatabase.contactDao();
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                Contact a = new Contact("Huỳnh Thị Ái Linh", "0768796932", "huynhthiailinh2105@gmail.com");
+                contactDao.insertAll(a);
+                contacts = (ArrayList<Contact>) contactDao.getAllContacts();
+            }
+        });
+
+//        contacts = new ArrayList<>();
+//        contacts.add(new Contact("Huỳnh Thị Ái Linh", "0768796932", "huynhthiailinh2105@gmail.com"));
+//        contacts.add(new Contact("Huỳnh Thị Khánh Linh",  "0768796932", "huynhthiailinh2105@gmail.com"));
+//        contacts.add(new Contact("Trần Như Trí", "0768796932", "huynhthiailinh2105@gmail.com"));
+
+        contactsAdapter = new ContactsAdapter(contacts);
+        binding.rvContacts.setAdapter(contactsAdapter);
+        binding.rvContacts.setLayoutManager(new LinearLayoutManager(this));
     }
 }
